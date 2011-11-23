62f2f3e697c37b8afe641cb01976092bb18f0acd
Sheldon Hearn <sheldonh@starjuice.net>
2011-11-23 15:46:36 +0200

    Add IPAddress.parse

--- lib/ipaddress.rb
+++ lib/ipaddress.rb
@@ -1,7 +1,13 @@
 require 'ipaddress/version'
 require 'ipaddress/v4'
 require 'ipaddress/v6'
-require 'ipaddress/base'
 
 module IPAddress
+  def self.parse(string)
+    if string.include?(':')
+      IPAddress::V6.new(string)
+    else
+      IPAddress::V4.new(string)
+    end
+  end
 end
